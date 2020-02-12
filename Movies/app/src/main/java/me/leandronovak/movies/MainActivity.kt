14b051279c5df8c11b8593e1631682e0a3b3d50d
Leandro Novak <leandro.novak@outlook.com>
2020-02-12 20:00:24 -0300

    Add base project

---
+++ Movies/app/src/main/java/me/leandronovak/movies/MainActivity.kt
@@ -0,0 +1,12 @@
+package me.leandronovak.movies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
